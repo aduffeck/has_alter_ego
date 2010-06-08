7d3d719153d0eb0b684d4046f473b78ef1dab8b0
Andre Duffeck <aduffeck@suse.de>
2010-06-08 11:18:42 +0200

    add has_schizophrenia class method and stub method for parsing the yml representation

--- lib/has_schizophrenia.rb
+++ lib/has_schizophrenia.rb
@@ -1,1 +1,31 @@
-# HasSchizophrenia
+module Schizophrenia
+  module ActiveRecordAdapater
+    def self.included(base)
+      base.send :extend, ClassMethods
+    end
+
+    module ClassMethods
+      def has_schizophrenia options = {}
+        class_eval do
+
+          send :include, InstanceMethods
+          parse_yml_representation
+        end
+      end
+
+      def parse_yml_representation
+        yml = File.open( File.join(RAILS_ROOT, "db", "fixtures", "schizophrenia", self.name.pluralize.downcase + ".yml") ) do |yf|
+          YAML::load( yf )
+        end
+        yml
+      end
+    end
+
+    module InstanceMethods
+    end
+  end
+end
+
+class ActiveRecord::Base
+  include Schizophrenia::ActiveRecordAdapater
+end
