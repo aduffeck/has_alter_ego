eb082e4797d77dd4f86f739b5c3ce8593f8ebc22
Andre Duffeck <aduffeck@suse.de>
2010-06-08 11:46:08 +0200

    reserver the first n IDs for stubbed objects

--- lib/schizophrenia.rb
+++ lib/schizophrenia.rb
@@ -7,9 +7,25 @@ module Schizophrenia
     module ClassMethods
       def has_schizophrenia options = {}
         class_eval do
+          options.reverse_merge!(:reserved_space => 1000)
 
           send :include, InstanceMethods
+          reserve_space(options[:reserved_space])
           parse_yml_representation
+        end
+      end
+
+      # Reserve the first n IDs for stubbed objects
+      def reserve_space space
+        max_id = Car.connection.execute("SELECT id FROM #{Car.name.pluralize.downcase} ORDER BY id DESC LIMIT 1").first["id"].to_i
+        return if max_id >= space
+        case self.connection.adapter_name
+        when "SQLite"
+          self.connection.execute("UPDATE SQLITE_SEQUENCE SET seq = #{space} WHERE name = '#{self.name.pluralize.downcase}'")
+        when "PostgreSQL"
+          self.connection.execute("SELECT setval('#{self.name.pluralize.downcase}', #{space});")
+        when "MySQL"
+          self.connection.execute("ALTER TABLE #{self.name.pluralize.downcase} AUTO_INCREMENT=#{space}")
         end
       end
 
