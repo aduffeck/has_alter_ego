6c5f4a2a5b212f226666bf89f1f1f7cec849b243
Andre Duffeck <aduffeck@suse.de>
2010-06-09 10:42:25 +0200

    create 'db/fixtures/schizophrenia' directory

--- generators/schizophrenia/schizophrenia_generator.rb
+++ generators/schizophrenia/schizophrenia_generator.rb
@@ -1,6 +1,7 @@
 class SchizophreniaGenerator < Rails::Generator::Base
   def manifest
     record do |m|
+      m.directory('db/fixtures/schizophrenia')
       m.migration_template('create_schizophrenics.rb', "db/migrate", :migration_file_name => 'create_schizophrenics')
     end
   end
